76a68d69cbe00bca4315420b9be0daeba9f1bd81
Benshakalaka <benstep222@gmail.com>
2016-11-04 13:00:16 +0800

    简单修改

--- HJSpider.py
+++ HJSpider.py
@@ -10,6 +10,7 @@ import json
 import util
 import re
 import logging
+import logging.config
 import configparser
 
 # 两个问题
@@ -21,38 +22,9 @@ class Spider(object):
     def __init__(self, userCode):
         self.config = configparser.ConfigParser()
         self.config_privacy = configparser.ConfigParser()
+        # 用户名密码配置文件username,password两个属性
         self.config_privacy.read('ConfigUser.conf', encoding='utf-8')
         self.config.read('ConfigHJ.conf', encoding='utf-8')
-
-        # 日志配置
-        # 一般在简单的小脚本中才会用logging.basicConfig，因为稍大些每个模块的logger就需要分开
-        # basicConfig配置后，会默认添加一个StreamHandler的，且获取的名为root的logger
-        # 可参考 http://www.jb51.net/article/52022.htm
-        # 输出到控制台的handler
-        CHnadler = logging.StreamHandler()
-        # 消息级别为WARNING及以上（级别分别是： NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL）
-        CHnadler.setLevel(logging.INFO)
-        # 输出到文件的handler
-        FHandler = logging.FileHandler(filename='hjspider.log',
-                                       mode='w',
-                                       encoding = 'UTF-8')
-        # 消息级别为DEBUG
-        FHandler.setLevel(logging.DEBUG)
-        # 设置格式
-        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s:  %(message)s')
-        # 设置格式
-        CHnadler.setFormatter(formatter)
-        FHandler.setFormatter(formatter)
-
-        # 获取一个hjspider的logger（每个模块的logger都可以有一个名字，如果不指定则为root）
-        self.logger = logging.getLogger('hjspider')
-        # 将handler附加在这个logger上（handler可以理解为消息先传到logger，然后在传给每个handler处理）
-        self.logger.addHandler(CHnadler)
-        self.logger.addHandler(FHandler)
-        # 既然消息是传到handler的，那么如果这个logger的level就和handler的level息息相关
-        # 比如logger的level为info（默认）, 那么即使handler的level设置为debug，也不可能得到debug的消息
-        # 每个handler的level在logger指定的level的基础上继续进行筛选
-        self.logger.setLevel(logging.DEBUG)
 
         #数据库配置
         self.mysql_user = self.config.get('MYSQL', 'user')
@@ -131,6 +103,38 @@ class Spider(object):
         except Exception as e:
             self.logger.error('各种限制请使用整型或是整型字符串！')
             exit(-1)
+
+    # 日志初始化
+    def loggerInit(self):
+        # 日志配置
+        # 一般在简单的小脚本中才会用logging.basicConfig，因为稍大些每个模块的logger就需要分开
+        # basicConfig配置后，会默认添加一个StreamHandler的，且获取的名为root的logger
+        # 可参考 http://www.jb51.net/article/52022.htm
+        # 输出到控制台的handler
+        CHnadler = logging.StreamHandler()
+        # 消息级别为WARNING及以上（级别分别是： NOTSET, DEBUG, INFO, WARNING, ERROR, CRITICAL）
+        CHnadler.setLevel(logging.INFO)
+        # 输出到文件的handler
+        FHandler = logging.FileHandler(filename='hjspider.log',
+                                       mode='w',
+                                       encoding='UTF-8')
+        # 消息级别为DEBUG
+        FHandler.setLevel(logging.DEBUG)
+        # 设置格式
+        formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s:  %(message)s')
+        # 设置格式
+        CHnadler.setFormatter(formatter)
+        FHandler.setFormatter(formatter)
+
+        # 获取一个hjspider的logger（每个模块的logger都可以有一个名字，如果不指定则为root）
+        self.logger = logging.getLogger('hjspider')
+        # 将handler附加在这个logger上（handler可以理解为消息先传到logger，然后在传给每个handler处理）
+        self.logger.addHandler(CHnadler)
+        self.logger.addHandler(FHandler)
+        # 既然消息是传到handler的，那么如果这个logger的level就和handler的level息息相关
+        # 比如logger的level为info（默认）, 那么即使handler的level设置为debug，也不可能得到debug的消息
+        # 每个handler的level在logger指定的level的基础上继续进行筛选
+        self.logger.setLevel(logging.DEBUG)
 
     # 用户登陆
     def userLogin(self):
@@ -863,3 +867,6 @@ if __name__ == "__main__":
 
 
     # body = requests.get('http://bulo.hujiang.com/u/54071261/', headers=headers, allow_redirects=False)
+
+
+
