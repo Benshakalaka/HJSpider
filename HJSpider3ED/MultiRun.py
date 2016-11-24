e6e7739c1c9f96ea10b03694da0f61e35574f582
Ben <benstep222@gmail.com>
2016-11-24 23:19:49 +0800

    多线程主程序

---
+++ HJSpider3ED/MultiRun.py
@@ -0,0 +1,23 @@
+from views.ListenUsers import ListenUsers
+from threading import Thread
+import HJSpider
+
+
+class UserInfoGetter(Thread):
+    def __init__(self):
+        super(UserInfoGetter, self).__init__()
+
+    def run(self):
+        pass
+
+
+class UserUidGetter(Thread):
+    def __init__(self):
+        super(UserInfoGetter, self).__init__()
+
+
+    def run(self):
+        pass
+
+if __name__ == '__main__':
+    pass
