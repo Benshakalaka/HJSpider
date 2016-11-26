f65ced814a3fcc48042da7ce2e87b5d2dfc8e896
Benshakalaka <benstep222@gmail.com>
2016-11-26 21:01:23 +0800

    Delete MultiRun.py
    无用文件

--- HJSpider3ED/MultiRun.py
+++
@@ -1,8 +0,0 @@
-from views.ListenUsers import ListenUsers
-from threading import Thread,current_thread
-from queue import Queue
-from threads.HJSpider import Spider
-from util import Utils
-
-if __name__ == '__main__':
-    Utils.loggerInit('hjspider')
